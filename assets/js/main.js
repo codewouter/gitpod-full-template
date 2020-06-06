1789764f1798755c026cbe96d12b4d326b184326
codewouter <wpc_okhuijsen@hotmail.com>
2020-06-06 14:25:40 +0000

    further js fiddling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
+console.log('Start of JS file');
 let orc = {
-    race:"orc"
-    currentHP: 15;
-    maxHP: 30;
+    race:"orc",
+    currentHP: 15,
+    maxHP: 30,
+    imgFile: '/assets/images/orc.jpg'
 }
+console.log('working!');
+document.getElementById("orc3").innerHTML = orc.race;
+$("#enemyPicture3").click(function() {
+    console.log('function entered');
+    document.getElementById("enemyPicture3").src = orc.imgFile;
+})
